17348a4a87d0a2ac2462544b97f44215bb2a6da9
redundant4u <rafch@naver.com>
2021-01-08 17:20:42 +0900

    [S] Add bluetooth part

--- first/README.md
+++ first/README.md
@@ -11,9 +11,13 @@
 - [charts_flutter](https://pub.dev/packages/charts_flutter)을 이용하여 그래프 페이지 구성(FourthPage).
 - 그래프 점을 누르면 Tooltip 같은 정보 표시.
 - 페이지별로 title이 변하도록 설정.
-- [volume_control](https://pub.dev/packages/volume_control)을 이용하여 SecondPage에 간단하게 volume 조절하는 함수 구현.
+- ~~[volume_control](https://pub.dev/packages/volume_control)을 이용하여 SecondPage에 간단하게 volume 조절하는 함수 구현.~~
 
 ## 210106
 - FirstPage는 TabBar 없이 구성하기 위해 Page에 있는 내용들을 수정함.
 - [flutter_xlider](https://pub.dev/packages/flutter_xlider)를 이용하여 SecondPage에 slider를 만들어 놓음. 기존에 있던 volume 조절 함수는 삭제.
 - 백그라운드 색깔을 white로 통일. appbar, body, bottomNavigationBar 경계 제거.
+
+## 210108
+- [flutter_blue](https://pub.dev/packages/flutter_blue)를 이용하여 FirstPage에 간단한 블루투스 기기들의 스캔과 연결하는 페이지 구성.
+- '''Unhandled Expection: setState() called after dispose()''' 오류가 나서 추후에 조치 필요.
--- first/lib/pages/FirstPage.dart
+++ first/lib/pages/FirstPage.dart
@@ -1,9 +1,128 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blue/flutter_blue.dart';
 
-class FirstPage extends StatelessWidget {
+class FirstPage extends StatefulWidget {
+  final FlutterBlue flutterBlue = FlutterBlue.instance;
+  final List<BluetoothDevice> deviceList = [];
+
+  @override
+  FirstPageState createState() => new FirstPageState();
+}
+
+class FirstPageState extends State<FirstPage> {
+  String _isScanning;
+  BluetoothDevice _connectedDevice;
+  List<BluetoothService> _services;
+
+  void initState() {
+    super.initState();
+    _isScanning = "";
+  }
+
+  _addDeviceToList(final BluetoothDevice device) {
+    if( !widget.deviceList.contains(device) )
+      setState(() { if(this.mounted) widget.deviceList.add(device); });
+  }
+
+  ListView _buildView() {
+    List<Container> containers = [];
+    for( BluetoothDevice device in widget.deviceList ) {
+      containers.add(
+        Container(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Column(
+                  children: <Widget>[
+                    Text(device.name == '' ? '(unknown device)' : device.name),
+                    Text(device.id.toString()),
+                  ],
+                ),
+
+              FlatButton(
+                child: Text("Connect"),
+                color: Colors.grey,
+                onPressed: () async {
+                  setState(() { _isScanning = "Connecting..."; });
+                  try { await device.connect();  }
+                  catch(e) { print(e); }
+                  finally {
+                    _services = await device.discoverServices();
+                    setState(() { _connectedDevice = device; _isScanning = "${device.name} Connect"; });
+                  }
+                },
+              )
+            ],
+          )
+        )
+      );
+    }
+
+    return ListView(
+      padding: EdgeInsets.all(10.0),
+      children: <Widget>[
+        Text(_isScanning),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            MaterialButton(
+              child: Text('Scan'),
+              color: Colors.grey,
+              onPressed: () { _isScanning = "Scanning..."; _startScan(); setState(() {}); }
+            ),
+
+            MaterialButton(
+              child: Text('Check'),
+              color: Colors.grey,
+              onPressed: () {
+                if( _connectedDevice != null ) _isScanning = "${_connectedDevice.name}";
+                else _isScanning = "";
+
+                setState(() {});
+              }
+            ),
+
+            MaterialButton(
+              child: Text('Stop'),
+              color: Colors.grey,
+              onPressed: () { _isScanning = "Stop Scanning"; _stopScan(); setState(() {}); }
+            ),
+          ],
+        ),
+
+        ...containers
+      ],
+    );
+  }
+
+  void _startScan() {
+    try {
+      widget.flutterBlue.startScan();
+      widget.flutterBlue.connectedDevices.asStream().listen((List<BluetoothDevice> devices) {
+        for( BluetoothDevice device in devices ) _addDeviceToList(device);
+      });
+      widget.flutterBlue.scanResults.listen((List<ScanResult> results) {
+        for( ScanResult result in results ) _addDeviceToList(result.device);
+      });
+    }
+
+    catch(e) { print(e); }
+  }
+
+  void _stopScan() {
+    if( _connectedDevice != null ) {
+      _connectedDevice.disconnect(); _connectedDevice = null;
+      _services.clear();
+    }
+
+    widget.deviceList.clear();
+    widget.flutterBlue.stopScan();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('hi')
+    return Scaffold(
+      body: _buildView(),
     );
   }
 }
